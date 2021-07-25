80d10daf2fbac357872eb946f14bb23b354c0197
Christopher Miller <admin@mctherealm.net>
2021-07-25 18:59:45 -0400

    Cleaned up ofs support target

--- ofs-support/target/debug/deps/ofs_support-1ae5b457f4f82f37.d
+++
@@ -1,7 +0,0 @@
-/home/chris/Repos/ofs/ofs-support/target/debug/deps/ofs_support-1ae5b457f4f82f37.rmeta: src/lib.rs src/fightstick.rs src/usart.rs
-
-/home/chris/Repos/ofs/ofs-support/target/debug/deps/ofs_support-1ae5b457f4f82f37.d: src/lib.rs src/fightstick.rs src/usart.rs
-
-src/lib.rs:
-src/fightstick.rs:
-src/usart.rs:
